b59bf339df036f65c5e7135432bcc5df3da1ca4a
Jitu-77 <jyotirmoypal077@gmail.com>
2025-04-16 23:47:29 +0530

    input box css changed

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -2,14 +2,14 @@
 
 import type React from "react"
 import { useState } from "react"
-import { IonContent, IonPage, IonInput, IonButton, IonText, IonItem, IonIcon, IonCol, IonGrid, IonRow } from "@ionic/react"
+import { IonContent, IonPage, IonInput, IonButton, IonText, IonItem, IonIcon, IonCol, IonGrid, IonRow, IonLabel } from "@ionic/react"
 
 import "../assets/styles/main.css"
 import "./Login.css"
 import InputBox from "../components/Input"
 import { Link, useHistory } from 'react-router-dom'
 import Header from "../components/Header"
-
+import call from '../../src/assets/images/call.png'
 const Login: React.FC = () => {
   const [mobileNumber, setMobileNumber] = useState("")
   const history = useHistory<History>();
@@ -17,6 +17,11 @@ const Login: React.FC = () => {
   const handleLogin = () => {
     history.push('/login-otp'); // Replace with your desired route
   };
+
+    const [solidValue, setSolidValue] = useState('');
+  const [outlineValue, setOutlineValue] = useState('');
+
+
   return (
     <IonPage>
       {/* <StatusBar/> */}
@@ -33,7 +38,7 @@ const Login: React.FC = () => {
           <IonRow className="mt-3x">
             <IonCol>
 
-              <IonInput
+              {/* <IonInput
                 // className={`${isValid && 'ion-valid'} ${isValid === false && 'ion-invalid'} ${isTouched && 'ion-touched'}`}
                 type="tel"
                 fill="outline"
@@ -43,7 +48,15 @@ const Login: React.FC = () => {
                 errorText="Invalid email"
                 className="custom-input"
                 placeholder=""
-              />
+              /> */}
+
+                <InputBox label="Mobile Number" defaultValue="Enter Your Mobile Number"
+                type="number" icon={<img
+                  src={call}
+                  alt="phone"
+                  style={{ width: 24, height: 24 }}
+                />}
+                inputProps={{ maxLength: 10, inputMode: "numeric",  pattern: "^[6-9][0-9]{9}$" }}/>
 
             </IonCol>
           </IonRow>
