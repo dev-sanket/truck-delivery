421a561835b61ea1317fe6a99f2aacc402109e23
Jitu-77 <jyotirmoypal077@gmail.com>
2025-04-29 00:35:27 +0530

    load details 2 done

--- src/pages/app/Bids.tsx
+++ src/pages/app/Bids.tsx
@@ -11,6 +11,10 @@ import {
   IonSelect,
   IonRow,
   IonCol,
+  IonCard,
+  IonCardContent,
+  IonInput,
+  IonGrid,
 } from "@ionic/react";
 import {
   arrowBack,
@@ -34,20 +38,14 @@ const PlaceBid: React.FC = () => {
     <IonPage>
       <LoadDetailsHeader />
       <IonContent className="ion-padding mt-2x">
-        
-        <div className="carrier-card">
-          {/* Route Information */}
+        {/* <div className="carrier-card">
           <div className="route-info">
             <div className="route-point-bid">
               <div className="info-label">From</div>
               <div className="location-text">Kharagpur, West Bengal</div>
             </div>
             <div className="arrow">
-            <img
-                  src={arrow}
-                  alt="arrow"
-                  style={{ width: 16, height: 16 }}
-                />
+              <img src={arrow} alt="arrow" style={{ width: 16, height: 16 }} />
             </div>
             <div className="route-point-bid">
               <div className="info-label">To</div>
@@ -55,24 +53,23 @@ const PlaceBid: React.FC = () => {
             </div>
           </div>
 
-          {/* Truck Type */}
 
           <div className="material-info">
             <div className="info-container">
               <div className="info-label">Delivery By</div>
-              {/* <div className="info-label">Total Ton</div> */}
             </div>
             <div className="product-container-load">
               <div className="material-type-bid">April 23 10:30AM</div>
-              {/* <div className="material-type-bid">35 TON</div> */}
             </div>
           </div>
-
 
           <div className="truck-info">
             <div className="info-label">Truck Types</div>
             <div>
-              <div className="truck-container"  style={{  justifyContent: 'unset', }}>
+              <div
+                className="truck-container"
+                style={{ justifyContent: "unset" }}
+              >
                 <img
                   src={miniPickup}
                   alt="phone"
@@ -84,16 +81,12 @@ const PlaceBid: React.FC = () => {
             </div>
           </div>
 
-          {/* Material Type */}
-
           <div className="material-info">
             <div className="info-container">
               <div className="info-label">Products</div>
-              {/* <div className="info-label">Total Ton</div> */}
             </div>
             <div className="product-container-load">
               <div className="material-type-bid">Aesbestos sheet</div>
-              {/* <div className="material-type-bid">35 TON</div> */}
             </div>
           </div>
 
@@ -118,31 +111,149 @@ const PlaceBid: React.FC = () => {
               <div className="material-type-bid">₹5,000</div>
             </div>
           </div>
+        </div> */}
 
-        </div>
+        <IonCard className="carrier-card ion-padding-top ion-no-padding">
+          <IonCardContent className="ion-no-padding">
+            <IonGrid className="ion-no-padding">
+              <IonRow>
+                <IonCol size="12">
+                  <div className="route-info">
+                    <div className="route-point-bid">
+                      <div className="info-label">From</div>
+                      <div className="location-text">
+                        Kharagpur, West Bengal
+                      </div>
+                    </div>
+                    <div className="arrow">
+                      <img
+                        src={arrow}
+                        alt="arrow"
+                        style={{ width: 16, height: 16 }}
+                      />
+                    </div>
+                    <div className="route-point-bid">
+                      <div className="info-label">To</div>
+                      <div className="location-text">
+                        Jalpaiguri, West Bengal
+                      </div>
+                    </div>
+                  </div>
+                </IonCol>
+              </IonRow>
+              <IonRow className="ion-padding-top">
+                <IonCol size="12">
+                  <div className="material-info">
+                    <div className="info-container">
+                      <div className="info-label">Delivery By</div>
+                    </div>
+                    <div className="product-container-load">
+                      <div className="material-type-bid">April 23 10:30AM</div>
+                    </div>
+                  </div>
+                </IonCol>
+              </IonRow>
+              <IonRow className="ion-padding-top">
+                <IonCol size="12">
+                  <div className="truck-info">
+                    <div className="info-label">Truck Types</div>
+                    <div>
+                      <div
+                        className="truck-container"
+                        style={{ justifyContent: "unset" }}
+                      >
+                        <img
+                          src={miniPickup}
+                          alt="phone"
+                          style={{ width: 31, height: 31 }}
+                        />
 
-        <div className="carrier-card pt-1x">
-          <div className="carrier-info">
-            <div className="carrier-avatar">
-              <img
-                src={userIcon}
-                alt="phone"
-                style={{ width: 40, height: 40, borderRadius: 50 }}
-              />
-            </div>
-            <div className="carrier-details-card ">
-              <div className="carrier-container">
-                <div className="carrier-name">Goyam Road Carriers</div>
-                {/* <div className="carrier-name-small">Abdol Sattar Gayen</div> */}
-              </div>
-              <div className="carrier-rating-card">
-                <span className="stars">★★★★★</span>
-                <span className="rating-value">4.5</span>
-              </div>
-            </div>
-          </div>
-        </div>
+                        <div className="carrier-name">Open Half/Full Body</div>
+                      </div>
+                    </div>
+                  </div>
+                </IonCol>
+              </IonRow>
+              <IonRow className="ion-padding-top">
+                <IonCol size="12">
+                  <div className="material-info">
+                    <div className="info-container">
+                      <div className="info-label">Products</div>
+                    </div>
+                    <div className="product-container-load">
+                      <div className="material-type-bid">Aesbestos sheet</div>
+                    </div>
+                  </div>
+                </IonCol>
+              </IonRow>
+              <IonRow className="ion-padding-top">
+                <IonCol size="12">
+                  <div className="material-info">
+                    <div className="info-container">
+                      <div className="info-label">Payment Terms</div>
+                      <div className="info-label">Total Ton</div>
+                    </div>
+                    <div className="product-container-load">
+                      <div className="material-type-bid">Advance</div>
+                      <div className="material-type-bid">35 TON</div>
+                    </div>
+                  </div>
+                </IonCol>
+              </IonRow>
+              <IonRow className="ion-padding-top">
+                <IonCol size="12">
+                  <div className="material-info">
+                    <div className="info-container">
+                      <div className="info-label">Past Date</div>
+                      <div className="info-label">Rate</div>
+                    </div>
+                    <div className="product-container-load">
+                      <div className="material-type-bid">April 22 10:30PM</div>
+                      <div className="material-type-bid">₹5,000</div>
+                    </div>
+                  </div>
+                </IonCol>
+              </IonRow>
+            </IonGrid>
+          </IonCardContent>
+        </IonCard>
 
+        <IonCard className="carrier-card pt-1x ion-no-padding">
+          <IonCardContent className="ion-no-padding">
+            <IonRow>
+              <IonCol size="12">
+                <IonInput
+                  // className={`${isValid && 'ion-valid'} ${isValid === false && 'ion-invalid'} ${isTouched && 'ion-touched'}`}
+                  type="text"
+                  fill="outline"
+                  label="From"
+                  labelPlacement="floating"
+                  errorText=""
+                  className="custom-input"
+                  placeholder="Enter a Source"
+                  helperText=""
+                  mode="md"
+                ></IonInput>
+              </IonCol>
+            </IonRow>
+            <IonRow className="pt-2x">
+              <IonCol size="12">
+                <IonInput
+                  // className={`${isValid && 'ion-valid'} ${isValid === false && 'ion-invalid'} ${isTouched && 'ion-touched'}`}
+                  type="text"
+                  fill="outline"
+                  label="To"
+                  labelPlacement="floating"
+                  errorText=""
+                  className="custom-input"
+                  placeholder="Enter a destination"
+                  helperText=""
+                  mode="md"
+                ></IonInput>
+              </IonCol>
+            </IonRow>
+          </IonCardContent>
+        </IonCard>
         <IonRow className="">
           <IonCol size="12">
             <IonButton
