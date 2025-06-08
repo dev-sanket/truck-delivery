ffbe534842b856b8febe2d451b439b27e5f356bc
Jitu-77 <jyotirmoypal077@gmail.com>
2025-06-08 13:00:10 +0530

    trip details is in WIP

--- src/components/TripCardDetails.tsx
+++ src/components/TripCardDetails.tsx
@@ -34,25 +34,14 @@ const TripCardDetails: React.FC<LoadCarrierDetailsProps> = ({
           <IonRow className="ion-justify-content-between">
             <IonCol size="12">
               <div className="carrier-info">
-                <div className="carrier-avatar">
-                  <img
-                    src={userIcon}
-                    alt="phone"
-                    style={{
-                      width: "100%",
-                      height: "100%",
-                      borderRadius: "50%",
-                    }}
-                  />
+                <div className="carrier-details">
+                  <div className="carrier-container">
+                    <div className="carrier-name">WB42V9307</div>
+                  </div>
                 </div>
                 <div className="carrier-details">
                   <div className="carrier-container">
-                    <div className="carrier-name">{data?.FullName}</div>
-                    <div className="carrier-name-small">{data?.FullName}</div>
-                  </div>
-                  <div className="carrier-rating">
-                    <span className="stars">★★★★★</span>
-                    <span className="rating-value">4.5</span>
+                    <div className="carrier-name">Apr 25</div>
                   </div>
                 </div>
               </div>
@@ -67,7 +56,7 @@ const TripCardDetails: React.FC<LoadCarrierDetailsProps> = ({
                     alt="phone"
                     style={{ width: 18, height: 18, borderRadius: 50 }}
                   />
-                  <div className="location-text">{data?.LoadFrom}</div>
+                  <div className="location-text">Kharagpur,West Bengal</div>
                 </div>
                 <div className="vertical-dots">
                   <IonIcon
@@ -81,7 +70,7 @@ const TripCardDetails: React.FC<LoadCarrierDetailsProps> = ({
                     alt="phone"
                     style={{ width: 18, height: 18, borderRadius: 50 }}
                   />
-                  <div className="location-text">{data?.LoadTo}</div>
+                  <div className="location-text">Jalpaiguri,West Bengal</div>
                 </div>
               </div>
             </IonCol>
@@ -89,22 +78,10 @@ const TripCardDetails: React.FC<LoadCarrierDetailsProps> = ({
           <IonRow className="ion-justify-content-between ion-padding-top">
             <IonCol size="12">
               <div className="truck-info">
-                <div className="info-label">Truck Types</div>
-                <div className="truck-container">
-                  <img
-                    src={miniPickup}
-                    alt="phone"
-                    style={{ width: 31, height: 31 }}
-                  />
-                  <div className="carrier-name" style={{ fontSize: 12 }}>Open Half/Full Body</div>
-                  <div className="weighIcon">
-                    <img
-                      src={weighIcon}
-                      alt="phone"
-                      style={{ width: 20, height: 20 }}
-                    />
-                    <div className="carrier-name" style={{ fontSize: 12 }}>{data?.ProductWeight}</div>
-                  </div>
+                <div className="wt-info-label">29.0MT</div>
+                <div className="trip-container">
+                  <div className="wt-info-label" style={{ fontSize: 14 }}>UAL Industries (UAL BENGAL)</div>
+                  <div className="wt-info-label" style={{ fontSize: 14 }}>AC Sheet</div>
                 </div>
               </div>
             </IonCol>
@@ -113,45 +90,48 @@ const TripCardDetails: React.FC<LoadCarrierDetailsProps> = ({
             <IonCol size="12">
               <div className="material-info">
                 <div className="info-container">
-                  <div className="info-label-section">Product</div>
-                  <div className="info-label-section">Payment Terms</div>
+                <div className="display-col" style={{ alignItems: 'center' }}>
+                  <IonButton
+                    expand="block"
+                    className="call-button"
+                    style={{
+                      width: "80px",
+                      height: "30px",
+                      "--background": "#FFD901",
+                      color: "#000000",
+                      fontSize:"12px"
+                    }}
+                  >
+                    In-Transit
+                  </IonButton>
                 </div>
-                <div className="product-container-load">
-                  <div className="material-type">Advance</div>
-                  <div className="material-type">Asbestos sheet</div>
+                <div className="display-col" style={{ alignItems: 'center' }}>
+                  <IonButton
+                    expand="block"
+                    className="call-button"
+                    style={{
+                      width: "80px",
+                      height: "30px",
+                      "--background": "#008000",
+                      color: "#FFFFFF",
+                      fontSize:"12px"
+                    }}
+                  >
+                    Actions
+                  </IonButton>
+                </div>
                 </div>
               </div>
             </IonCol>
           </IonRow>
           <IonRow className="ion-justify-content-between ion-padding-top">
             <IonCol size="12">
-              <div className="rate-info">
-                <div className="display-col">
-                  <div className="rate-label">Rate</div>
-                  <div className="product-container-load" style={{ alignItems: 'unset' }}>
-                    <div className="rate-value">
-                      ₹2,000{" "}
-                      <span style={{ fontSize: "16px", fontWeight: 400, color: '#000000' }}>
-                        per ton{" "}
-                      </span>
-                    </div>
-                  </div>
+              <div className="trip-card-footers">
+                <div className="trip-under-lined">
+                  Read Notes
                 </div>
-                <div className="display-col" style={{ alignItems: 'center' }}>
-                  <IonButton
-                    expand="block"
-                    className="call-button"
-                    onClick={() => router.push(`/app/place-bid/${data?.LoadsID}`)}
-                    style={{
-                      width: "80px",
-                      height: "30px",
-                      "--background": "#FFD901",
-                      color: "#000000",
-                    }}
-                  >
-                    Bid
-                  </IonButton>
-                  {showLabel && <div className="label-l1">L1</div>}
+                <div className="wt-info-label">
+                  Reporting time:25 Apr,2:30PM
                 </div>
               </div>
             </IonCol>
